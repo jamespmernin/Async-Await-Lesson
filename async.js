7f992978bc8410ff1e4e320111a73c3fbc91230f
James Mernin <jamespmernin@gmail.com>
2020-09-17 15:40:34 -0400

    Got avatar and name to appear

--- async.js
+++ async.js
@@ -1,18 +1,29 @@
-  function showAvatar(name) {
+async function showAvatar(name) {
   // read github user
-    let githubUser = axios.get(`https://api.github.com/users/${name}`);
-  
+  try {
+    let githubUser = await axios.get(`https://api.github.com/users/${name}`);
+    console.log(githubUser);
+
     // show the avatar
     document.getElementById('useravatar').src = githubUser.data.avatar_url;
     document.getElementById('username').innerHTML = githubUser.data.name;
-  
+
     return githubUser;
 
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  } finally {
+    console.log('This has been resolved.');
+  }
+
+  axios.get(`https://api.github.com/users/${name}`).then((res) => {
+    console.log(res);
+  })
 }
 
-showAvatar('subtlesnow');
+showAvatar('JamesMernin');
 
- function showStargazers() {
+function showStargazers() {
   // read github user
   let githubUsers = axios.get(`
     https://api.github.com/repos/axios/axios/stargazers`);
